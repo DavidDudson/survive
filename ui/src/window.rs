04a923aec9e48eec8518fbb4ae0213767f283a54
David Dudson <davidjohndudson@gmail.com>
2024-07-12 21:16:49 +1000

    feat(window): Alter basic window plugin

---
+++ ui/src/window.rs
@@ -0,0 +1,21 @@
+use bevy::prelude::*;
+use bevy::window::*;
+
+const SCREEN_WIDTH: f32 = 1920.;
+const SCREEN_HEIGHT: f32 = 1080.;
+
+pub fn window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(Window {
+            title: String::from("Survive!"),
+            name: Some(String::from("survive.app")),
+            resolution: WindowResolution::from((SCREEN_WIDTH, SCREEN_HEIGHT)),
+            present_mode: PresentMode::AutoNoVsync,
+            fit_canvas_to_parent: true,
+            prevent_default_event_handling: false,
+            window_theme: Some(WindowTheme::Dark),
+            ..default()
+        }),
+        ..default()
+    }
+}
