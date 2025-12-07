8118fca4dbbcd6efb5b5bf41c66e69d870036493
David Dudson <david.dudson@gmail.com>
2025-12-07 10:13:36 +1000

    Fixes

--- models/src/scenery.rs
+++ models/src/scenery.rs
@@ -4,9 +4,5 @@ use bevy_rapier2d::dynamics::RigidBody;
 use bevy_rapier2d::prelude::Collider;
 
 #[derive(Component, Default)]
-#[require(RigidBody(scenery_body), Collider, Hardness)]
+#[require(Rigidbody::Fixed, Collider, Hardness)]
 pub struct Scenery;
-
-fn scenery_body() -> RigidBody {
-    RigidBody::Fixed
-}
